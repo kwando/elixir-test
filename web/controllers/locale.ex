9c90c04a106a53988f3f68156975db95bcdcb64d
kwando <kwando@merciless.me>
2017-02-08 09:33:47 +0100

    Some translations.

---
+++ web/controllers/locale.ex
@@ -0,0 +1,14 @@
+defmodule Test.Locale do
+  import Plug.Conn
+  
+  def init(_opts), do: nil
+
+  def call(conn, _opts) do
+    case conn.params["locale"] || get_session(conn, :locale) do
+       nil -> conn
+       locale ->
+         Gettext.put_locale(Test.Gettext, locale)
+         conn |> put_session(:locale, locale)
+    end
+  end
+end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Test.PageController do
   use Test.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", time: DateTime.utc_now()
   end
 end
